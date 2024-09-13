7af985dfd3f4350559dcf9bda76a93c5081f5c4b
RiccardoMacchi <rick.mac97@outlook.com>
2024-09-13 10:07:02 +0200

    add time and formatted time

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,20 +9,23 @@ class PageController extends Controller
 {
     public function index(){
         // where('departure_day','>','CURRENTDATE')
-        $all_trains = Train::where('departure_day','>', Train::raw('CURDATE()'))->get();
-        return view('home', compact('all_trains'));
+        $all_trains = Train::where('departure_day','>', Train::raw('CURDATE()'))->orderBy('departure_day')->get();
+        $title = 'Treni Odierni';
+        return view('home', compact('all_trains','title'));
     }
 
     public function allTrains(){
         // where('departure_day','>','CURRENTDATE')
         $all_trains = Train::all();
-        return view('home', compact('all_trains'));
+        $title = 'Registro di tutti i treni';
+        return view('home', compact('all_trains','title'));
     }
 
     public function enterpriseTrains(){
         // where('departure_day','>','CURRENTDATE')
         $all_trains = Train::orderBy('enterprise')->get();
-        return view('home', compact('all_trains'));
+        $title = 'Treni per Compagnie';
+        return view('home', compact('all_trains','title'));
     }
 
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,17 +6,20 @@
 @endsection
 
 @section('content')
-    <div class="container my-5">
-        <h1>titolo</h1>
+    <div class="container my-5 text-center">
+        <h1>{{ $title }}</h1>
         <table class="table">
             <thead>
                 <tr>
                     <th scope="col">ID TRENO</th>
                     <th scope="col">Compagnia</th>
+                    <th scope="col">Giorno</th>
                     <th scope="col">Stazione di partenza</th>
                     <th scope="col">Orario di partenza</th>
                     <th scope="col">Stazione di arrivo</th>
                     <th scope="col">Orario di arrivo</th>
+                    <th scope="col">Numero di Carrozze</th>
+                    <th scope="col">In Orario</th>
                 </tr>
             </thead>
             <tbody>
@@ -24,10 +27,18 @@
                     <tr>
                         <td>{{ $train->id }}</td>
                         <th>{{ $train->enterprise }}</th>
+                        {{-- @php
+                            $day_date = date_create($train->departure_day);
+                        @endphp
+                        <td>{{ date_format($day_date, 'd-m-Y') }}</td> --}}
+                        <td>{{ date('d-m-Y', strtotime($train->departure_day)) }}</td>
                         <td>{{ $train->departure_station }}</td>
                         <td>{{ $train->departure_hour }}</td>
                         <td>{{ $train->arrival_station }}</td>
                         <td>{{ $train->arrival_hour }}</td>
+                        <td>{{ $train->numbers_of_carriages }}</td>
+                        <td>{{ $train->on_time ? 'In Orario' : 'Ritardo' }}</td>
+
                     </tr>
                 @endforeach
             </tbody>
